bfb80e6d54b5e75b57c0ecce39063bafc6bbed51
rishijain544 <rishijain30a@gmail.com>
2026-03-26 22:03:47 +0530

    Deployment readiness: SQLite compatibility fix, Streamlit config, and requirements update

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 # Place robot image as "background.jpg" in project folder
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import streamlit as st
 import os
 import time
